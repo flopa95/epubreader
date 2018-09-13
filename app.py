29095e8cf1404a1464c45e16cf73702db50c964e
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-09-13 20:33:17 +0800

    fixed html rendering and added start for right page

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html', leftpage=openbook.read('C:\\Users\\jerry-chung\\Desktop\\EbookReader\\local_db\\Factfulness by Hans Rosling.epub'))
+    return render_template('index.html', leftpage=openbook.read('C:\\Users\\User\\Desktop\\Crazy Rich Asians.epub'),rightpage=openbook.read2('C:\\Users\\User\\Desktop\\Crazy Rich Asians.epub'))
 
 if __name__ == "__main__":
     app.run()
+
--- openbook.py
+++ openbook.py
@@ -12,18 +12,15 @@ def read(book_url):
         if item.get_type() == ebooklib.ITEM_DOCUMENT:
             chapter.append(item.get_body_content())
 
-    #banana = chapter[7].decode("utf-8")
-    #print (chapter)
-    return htmltext.html2text(chapter[1].decode("utf-8"))
+    return chapter[5].decode("utf-8")
+
+def read2(book_url):
+    book = epub.read_epub(book_url)
+
+    for item in book.get_items():
+        if item.get_type() == ebooklib.ITEM_DOCUMENT:
+            chapter.append(item.get_body_content())
+
+    return chapter[6].decode("utf-8")
 
 
-
-
-    '''
-for z in range(len(x)):
-    decoded = x[z].decode('utf8')
-    f = open(newpath +'/Chapter ' + str(z+1)+'.html','w',encoding='utf-8')
-    f.write(decoded)
-    f.close()
-
-    '''
