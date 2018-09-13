7d40eb25b1cc2ee34ef759a71b758362898a02f6
flopa95 <fparzhuber.2013@business.smu.edu.sg>
2018-09-13 20:48:23 +0800

    added in the spine for the right order

--- openbook.py
+++ openbook.py
@@ -3,24 +3,36 @@ import ebooklib
 import os
 import html2text as htmltext
 
-chapter = []
+chapter_list = []
+book_spine = []
 
 def read(book_url):
     book = epub.read_epub(book_url)
 
-    for item in book.get_items():
-        if item.get_type() == ebooklib.ITEM_DOCUMENT:
-            chapter.append(item.get_body_content())
+    for i in range (len(book.spine)):
+        book_spine.append(book.spine[i][0])
 
-    return chapter[5].decode("utf-8")
+    for j in range(len(book_spine)):
+        chapter = book.get_item_with_id(book_spine[j])
+        chapter_list.append(chapter.get_body_content())
+
+    return chapter_list[10].decode("utf-8")
 
 def read2(book_url):
     book = epub.read_epub(book_url)
 
-    for item in book.get_items():
-        if item.get_type() == ebooklib.ITEM_DOCUMENT:
-            chapter.append(item.get_body_content())
+    for i in range (len(book.spine)):
+        book_spine.append(book.spine[i][0])
 
-    return chapter[6].decode("utf-8")
+    for j in range(len(book_spine)):
+        chapter = book.get_item_with_id(book_spine[j])
+        chapter_list.append(chapter.get_body_content())
 
+    return chapter_list[11].decode("utf-8")
+
+
+
+
+
+### Retrieving all the HTML files of the book in correct order ###
 
